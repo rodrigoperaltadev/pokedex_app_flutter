4e822f1d9d852526d637197f615a176331b94a49
Rodrigo Peralta <rodrigo.peralta@valtech.com>
2022-06-05 18:03:00 -0300

    update main.dart

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_coco_version/routes/routes.dart';
+import 'package:pokedex_coco_version/themes/themes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PodeDex',
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
+      theme: AppTheme.defaultTheme,
+    );
+  }
+}
