bfc05fb53fba8ec5314d4a938605f9d1593e9016
Rodrigo Peralta <rodrigo.peralta@valtech.com>
2022-06-10 19:57:49 -0300

    Add assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex_coco_version/routes/routes.dart';
 import 'package:pokedex_coco_version/themes/themes.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
